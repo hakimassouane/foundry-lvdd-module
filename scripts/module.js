7e9782bb9695fe6c36a6a1f0d93a490073397c19
hakimassouane <hakim.assouane@epitech.eu>
2022-03-30 12:15:41 +0200

    init repo

---
+++ scripts/module.js
@@ -0,0 +1,7 @@
+Hooks.once('init', async function() {
+    console.log('init foundry lvdd module')
+});
+
+Hooks.once('ready', async function() {
+    console.log('foundry lvdd module is ready')
+});
